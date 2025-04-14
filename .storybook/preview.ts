dac41704a331cf816e862c4c388610cbd946eaa5
Cida Luna <cidalunna@hotmail.com>
2025-04-14 20:26:59 -0300

    Install storybook on Angular App

---
+++ .storybook/preview.ts
@@ -0,0 +1,20 @@
+import { Preview } from '@storybook/angular';
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+    docs: {
+      source: {
+        type: 'code', // Isso garante que o HTML renderizado apareça na aba Docs
+      },
+    },
+  },
+};
+
+export default preview;
