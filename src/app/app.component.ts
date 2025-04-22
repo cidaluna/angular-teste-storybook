044ece601f881b3d7fc1bac7a7d7cf0aca97101f
Cida Luna <cidalunna@hotmail.com>
2025-04-22 20:03:13 -0300

    Print name screen var on app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -10,4 +10,25 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'angular-teste-storybook';
+  screenType: 'small' | 'tablet' | 'desktop' = 'desktop';
+
+  ngOnInit() {
+    this.setScreenType(window.innerWidth);
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    const target = event.target as Window;
+    this.setScreenType(target.innerWidth);
+  }
+
+  private setScreenType(width: number): void {
+    if (width <= 599) {
+      this.screenType = 'small';
+    } else if (width >= 600 && width < 1024) {
+      this.screenType = 'tablet';
+    } else {
+      this.screenType = 'desktop';
+    }
+  }
 }
